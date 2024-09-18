af89a3bc5434642e186b7757c49f0f591dad614c
Mehboob Raza <98275465+mehboob-raza@users.noreply.github.com>
2024-09-19 00:14:53 +0500

    adedd

---
+++ components/Appbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+// import { ThemeSwitcher } from "./ThemeSwitcher";
+
+export function Appbar({ showThemeSwitch = true }) {
+  const session = useSession();
+  const router= useRouter();
+  const isUserLoggedIn = session.data?.user;
+  const buttonTitle = isUserLoggedIn ? "Logout" : "Login";
+
+  const handleButtonClick = () => {
+    isUserLoggedIn
+      ? signOut({ callbackUrl: "/" })
+      : signIn("google", { callbackUrl: "/home" });
+  };
+
+  return (
+    <div className="flex justify-between px-5 py-4 md:px-10 xl:px-20">
+      <div
+        onClick={()=>{
+        router.push('/home') 
+      }}  className={`flex flex-col hover:cursor-pointer justify-center text-lg font-bold ${showThemeSwitch ? "" : "text-white"}`}
+      >
+        Muzer
+      </div>
+      <div className="flex items-center gap-2">
+        {/* {showThemeSwitch && <ThemeSwitcher />} */}
+        <Button onClick={handleButtonClick}>{buttonTitle}</Button>
+      </div>
+    </div>
+  );
+}
---
+++ components/Provider.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { SocketContextProvider } from "@/context/socket-context";
+import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <SocketContextProvider>
+        {children}
+      </SocketContextProvider>
+    </SessionProvider>
+  );
+}
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
